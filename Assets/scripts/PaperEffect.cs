7ad29e13bf1448e80153566b0dacee3f5594f09f
Yuji YASUHARA <yuji.yasuhara@gmail.com>
2016-01-31 08:19:37 +0900

    rollup effect

--- Assets/scripts/PaperEffect.cs
+++ Assets/scripts/PaperEffect.cs
@@ -5,8 +5,10 @@ using System.Collections;
 [RequireComponent (typeof (MeshRenderer))]
 public class PaperEffect : MonoBehaviour
 {
+	[Range(-1, 1)]
+	public float s = 0f;
 	private const float width = 0.667f;
-	private const float height = 2f;
+	private const float height = 1.5f;
 	private const int X_NUM = 16;
 	private const int Y_NUM = 32;
 
@@ -21,7 +23,8 @@ public class PaperEffect : MonoBehaviour
 		var uvs = new Vector2[X_NUM * Y_NUM];
 		for (int y = 0; y < Y_NUM; ++y) {
 			for (int x = 0; x < X_NUM; ++x) {
-				uvs [x + y * X_NUM] = new Vector2 (((float)x) / X_NUM, ((float)(y-(Y_NUM/4))*1.5f) / (Y_NUM));
+				// uvs [x + y * X_NUM] = new Vector2 (((float)x) / X_NUM, ((float)(y-(Y_NUM/4))*1.5f) / Y_NUM);
+				uvs [x + y * X_NUM] = new Vector2 (((float)x) / X_NUM, ((float)y) / Y_NUM);
 			}
 		}
 		const int RECT_NUM = (X_NUM - 1) * (Y_NUM - 1);
@@ -47,4 +50,45 @@ public class PaperEffect : MonoBehaviour
 		mesh.RecalculateBounds();
 		GetComponent<MeshFilter>().sharedMesh = mesh;
 	}
+
+	void Update()
+	{
+		const float R0 = 0.05f;
+		const float R1 = 0.06f;
+		float l = height;
+		var vertices = new Vector3[X_NUM * Y_NUM];
+		for (int y = 0; y < Y_NUM; ++y) {
+			for (int x = 0; x < X_NUM; ++x) {
+				float xi = (x*width/X_NUM) - width*0.5f;
+				float yi = (y*height/Y_NUM) - height*0.5f;
+				if (0 < s) {
+					float p = l * s;
+					float k = yi - (l*0.5f - p);
+					if (k <= 0) {
+						vertices[x + y * X_NUM] = new Vector3(xi, yi, 0f);
+					} else {
+						float r = (1f - (k/l)) * R0 + (k/l)*R1;
+						float yn = (l*0.5f - p) + r * (float)System.Math.Sin(k/r);
+						float zn = -R0 + r * (float)System.Math.Cos(k/r);
+						vertices[x + y * X_NUM] = new Vector3(xi, yn, zn);
+					}
+				} else if (s < 0) {
+					float p = -l * s;
+					float k = -(l*0.5f) + p - yi;
+					if (k <= 0) {
+						vertices[x + y * X_NUM] = new Vector3(xi, yi, 0f);
+					} else {
+						float r = (1f - (k/l)) * R0 + (k/l)*R1;
+						float yn = (-l*0.5f + p) + r * (float)System.Math.Sin(k/r);
+						float zn = -R0 + r * (float)System.Math.Cos(k/r);
+						vertices[x + y * X_NUM] = new Vector3(xi, yn, zn);
+					}
+				} else {
+					vertices[x + y * X_NUM] = new Vector3(xi, yi, 0f);
+				}
+			}
+		}
+		GetComponent<MeshFilter>().sharedMesh.vertices = vertices;
+	}
+
 }
