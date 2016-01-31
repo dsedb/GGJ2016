617bb4fd46efdf3aafd96f10844a659777e894db
Yuji YASUHARA <yuji.yasuhara@gmail.com>
2016-01-31 13:43:25 +0900

    tweak low panel and side panel

--- Assets/scripts/LowPanelController.cs
+++ Assets/scripts/LowPanelController.cs
@@ -3,26 +3,44 @@ using System.Collections;
 
 public class LowPanelController : MonoBehaviour {
 
-	UnityEngine.UI.Image image_;
-	float alpha_;
+	// UnityEngine.UI.Image image_;
+	UnityEngine.UI.Image bg_image_red_;
+	UnityEngine.UI.Image bg_image_blue_;
+	UnityEngine.UI.Image bg_image_green_;
+	// float alpha_;
 
 	void Awake()
 	{
-		image_ = GetComponent<UnityEngine.UI.Image>();
-		alpha_ = image_.color.a;
+		bg_image_red_ = transform.Find("bg_red").GetComponent<UnityEngine.UI.Image>();
+		bg_image_red_.enabled = true;
+		bg_image_blue_ = transform.Find("bg_blue").GetComponent<UnityEngine.UI.Image>();
+		bg_image_blue_.enabled = false;
+		bg_image_green_ = transform.Find("bg_green").GetComponent<UnityEngine.UI.Image>();
+		bg_image_green_.enabled = false;
+		// image_ = GetComponent<UnityEngine.UI.Image>();
+		// alpha_ = image_.color.a;
 	}
 
 	public void setElementType(ElementType element_type)
 	{
 		switch (element_type) {
 			case ElementType.Red:
-				image_.color = new Color(1, 0, 0, alpha_);
+				bg_image_red_.enabled = true;
+				bg_image_blue_.enabled = false;
+				bg_image_green_.enabled = false;
+				// image_.color = new Color(1, 0, 0, alpha_);
 				break;
 			case ElementType.Blue:
-				image_.color = new Color(0, 0, 1, alpha_);
+				bg_image_red_.enabled = false;
+				bg_image_blue_.enabled = true;
+				bg_image_green_.enabled = false;
+				// image_.color = new Color(0, 0, 1, alpha_);
 				break;
 			case ElementType.Green:
-				image_.color = new Color(0, 1, 0, alpha_);
+				bg_image_red_.enabled = false;
+				bg_image_blue_.enabled = false;
+				bg_image_green_.enabled = true;
+				// image_.color = new Color(0, 1, 0, alpha_);
 				break;
 		}
 	}
